3219aaffd0eb925e9331cff500c7013ea45ac303
Faithfultennyson-ESEGAMES <faithfultennyson@esegames.com>
2026-08-11 07:51:17 +0100

    Delete tests directory

--- tests/distributionRuntime.test.js
+++
@@ -1,9 +0,0 @@
-import test from 'node:test';
-import assert from 'node:assert/strict';
-import { getVisibleContainerIndexes } from '../../client/public/src/distributionRuntime.js';
-
-test('visible container hydration keeps nearby containers filled and distant containers empty', () => {
-  assert.deepEqual(getVisibleContainerIndexes(5, 0, 1), [0, 1]);
-  assert.deepEqual(getVisibleContainerIndexes(5, 2, 1), [1, 2, 3]);
-  assert.deepEqual(getVisibleContainerIndexes(5, 4, 1), [3, 4]);
-});
--- tests/run-all.js
+++
@@ -1,7 +0,0 @@
-await import('./contracts.test.js');
-await import('./domain.test.js');
-await import('./registry.test.js');
-await import('./runtime.test.js');
-await import('./security.test.js');
-await import('./twod.test.js');
-await import('./webhooks.test.js');
